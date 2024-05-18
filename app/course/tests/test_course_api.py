759714d19c9470ead2479add969477722c29464e
Mikhail Fedotov <95757128+Imenno-On@users.noreply.github.com>
2024-05-18 22:33:59 +0300

    Added some more tests for the recipe API

--- app/course/tests/test_course_api.py
+++ app/course/tests/test_course_api.py
@@ -64,9 +64,9 @@ class PrivateCourseAPITest(TestCase):
 
     def setUp(self):
         self.client = APIClient()
-        self.user = get_user_model().objects.create_user(
-            'user@example.com',
-            'testpass123',
+        self.user = create_user(
+            email='user@example.com',
+            password='testpass123'
         )
         self.client.force_authenticate(self.user)
 
@@ -84,9 +84,9 @@ class PrivateCourseAPITest(TestCase):
 
     def test_course_list_limited_to_user(self):
         """Test list of courses is limited to authenticated user."""
-        other_user = get_user_model().objects.create_user(
-            'other@example.com',
-            'password123',
+        other_user = create_user(
+            email='other@example.com',
+            password='password123'
         )
         create_course(user=other_user)
         create_course(user=self.user)
@@ -122,3 +122,80 @@ class PrivateCourseAPITest(TestCase):
         for k, v in payload.items():
             self.assertEqual(getattr(course, k), v)
         self.assertEqual(course.user, self.user)
+
+    def test_partial_update(self):
+        """Test partial update course."""
+        original_link = 'https://example.com/course'
+        course = create_course(
+            user=self.user,
+            title='Sample Course',
+            link=original_link,
+        )
+
+        payload = {'title': 'New Course Title'}
+        url = detail_url(course.id)
+        res = self.client.patch(url, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        course.refresh_from_db()
+        self.assertEqual(course.title, payload['title'])
+        self.assertEqual(course.link, original_link)
+        self.assertEqual(course.user, self.user)
+
+    def test_full_update(self):
+        """Test full update course."""
+        course = create_course(
+            user=self.user,
+            title='Sample course title',
+            link='https://example.com/course',
+            description='Sample course description.',
+        )
+
+        payload = {
+            'title': 'New Course Title',
+            'link': 'https://example.com/new-course',
+            'description': 'New course description.',
+            'duration_hours': 1000,
+            'price': Decimal('12.34'),
+        }
+        url = detail_url(course.id)
+        res = self.client.put(url, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        course.refresh_from_db()
+        for k, v in payload.items():
+            self.assertEqual(getattr(course, k), v)
+        self.assertEqual(course.user, self.user)
+
+    def test_update_user_returns_error(self):
+        """Test changing the recipe user returns error."""
+        new_user = create_user(email='user2@example.com', password='test12345')
+        course = create_course(user=self.user)
+
+        payload = {'user': new_user.id}
+        url = detail_url(course.id)
+        self.client.patch(url, payload)
+
+        course.refresh_from_db()
+        self.assertEqual(course.user, self.user)
+
+    def test_delete_course(self):
+        """Test deleting a course successful."""
+        course = create_course(user=self.user)
+
+        url = detail_url(course.id)
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Course.objects.filter(id=course.id).exists())
+
+    def test_delete_other_users_course_error(self):
+        """Test trying to delete other users course returns error."""
+        new_user = create_user(email='user2@example', password='pass12345')
+        course = create_course(user=new_user)
+
+        url = detail_url(course.id)
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertTrue(Course.objects.filter(id=course.id).exists())
