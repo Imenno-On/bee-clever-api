094d82fab743e9aa72773352c23f15b3d9b7bba0
Mikhail Fedotov <95757128+Imenno-On@users.noreply.github.com>
2024-05-30 17:37:20 +0300

    Update settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -44,9 +44,11 @@ INSTALLED_APPS = [
     'drf_spectacular',
     'user',
     'course',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -75,6 +77,12 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'app.wsgi.application'
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
 
 
 # Database
